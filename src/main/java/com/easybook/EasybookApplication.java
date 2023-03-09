f7ade8e08269ad81dafe3b10450df966b9a537a3
DESKTOP-JEM4E84\Lenovo <test>
2023-03-10 02:38:40 +0900

    aws 관련 환경 세팅

--- src/main/java/com/easybook/EasybookApplication.java
+++ src/main/java/com/easybook/EasybookApplication.java
@@ -2,9 +2,10 @@ package com.easybook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EasybookApplication {
+public class EasybookApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EasybookApplication.class, args);
